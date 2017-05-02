f3415ecdec0a065681f7848c4531769742a690f0
yurimann <yuri.manahan@gmail.com>
2017-05-02 14:44:57 -0400

    Created an add and delete component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -41,6 +41,22 @@ export class HeroService{
           .then(() => hero)
           .catch(this.handleError);
   }
+
+  create(name: string): Promise<Hero> {
+    return this.http
+          .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+          .toPromise()
+          .then(res => res.json().data as Hero)
+          .catch(this.handleError);
+  }
+
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+          .toPromise()
+          .then(() => null)
+          .catch(this.handleError);
+  }
   // getHeroesSlowly(): Promise<Hero[]> {
   //   return new Promise(resolve => {
   //     setTimeout(() => resolve(this.getHeroes()), 2000);
